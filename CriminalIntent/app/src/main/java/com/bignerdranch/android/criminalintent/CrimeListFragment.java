17d202fb33a79cde76415e82cd8321663868b61b
Joey Jirasevijinda <joey.jira@gmail.com>
2018-05-12 22:38:25 -0700

    added icon

--- CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
@@ -10,9 +10,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import org.w3c.dom.Text;
-
 import java.util.List;
 
 public class CrimeListFragment extends Fragment {
