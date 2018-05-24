95525284bffae7f00b3c26c984fc7dd5e32486a3
Joey Jirasevijinda <joey.jira@gmail.com>
2018-05-23 22:16:35 -0700

    added TimePicker Fragment

---
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/TimePickerFragment.java
@@ -0,0 +1,10 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.support.v4.app.DialogFragment;
+import android.widget.TimePicker;
+
+public class TimePickerFragment extends DialogFragment {
+
+    private TimePicker mTimePicker;
+
+}
