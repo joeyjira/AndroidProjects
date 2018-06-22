aabeda70153673b5e460aed9bb08d4decef78a23
Joey Jirasevijinda <joey.jira@gmail.com>
2018-06-22 10:39:17 -0700

    update and draw method

--- TappyDefender/app/src/main/java/com/septem/tappydefender/TDView.java
+++ TappyDefender/app/src/main/java/com/septem/tappydefender/TDView.java
@@ -1,6 +1,10 @@
 package com.septem.tappydefender;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 public class TDView extends SurfaceView implements Runnable {
@@ -10,9 +14,16 @@ public class TDView extends SurfaceView implements Runnable {
 
     // Game objects
     private PlayerShip player;
+    private Paint paint;
+    private Canvas canvas;
+    private SurfaceHolder ourHolder;
 
     public TDView(Context context) {
         super(context);
+        ourHolder = getHolder();
+
+        // Initialize player ship
+        player = new PlayerShip(context);
     }
 
     @Override
@@ -25,11 +36,23 @@ public class TDView extends SurfaceView implements Runnable {
     }
 
     private void update() {
-
+        player.update();
     }
 
     private void draw() {
+        // Lock area of memory that's drawn to
+        canvas = ourHolder.lockCanvas();
 
+        // Rub out the last frame
+        canvas.drawColor(Color.argb(255, 0, 0, 0));
+
+        // Draw the player
+        canvas.drawBitmap(
+                player.getBitmap(),
+                player.getX(),
+                player.getY(),
+                paint
+        );
     }
 
     private void control() {
