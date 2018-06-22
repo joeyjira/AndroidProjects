0ae1bf04996ff142c4be503917411596240f844f
Joey Jirasevijinda <joey.jira@gmail.com>
2018-06-22 14:12:43 -0700

    set min and max speed

--- TappyDefender/app/src/main/java/com/septem/tappydefender/PlayerShip.java
+++ TappyDefender/app/src/main/java/com/septem/tappydefender/PlayerShip.java
@@ -6,21 +6,60 @@ import android.graphics.BitmapFactory;
 
 public class PlayerShip {
     private Bitmap bitmap;
+
     private int x, y;
-    private int speed = 0;
+
+    private int mSpeed = 0;
 
     private boolean mBoosting;
+
+    private final int GRAVITY = -12;
+
+    // Stop ship leaving the screen
+    private int maxY;
+    private int minY;
+
+    //  Limit the bounds of the ship's speed
+    private final int MIN_SPEED = 1;
+    private final int MAX_SPEED = 20;
 
     public PlayerShip(Context context) {
         x = 50;
         y = 50;
-        speed = 1;
+        mSpeed = 1;
         bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.ship);
         mBoosting = false;
     }
 
     public void update() {
-        x++;
+        if (mBoosting) {
+            // Speed up
+            mSpeed += 2;
+        } else {
+            mSpeed -= 5;
+        }
+
+        // Constrain top speed
+        if (mSpeed > MAX_SPEED) {
+            mSpeed = MAX_SPEED;
+        }
+
+        // Never stop completely
+        if (mSpeed < MIN_SPEED) {
+            mSpeed = MIN_SPEED;
+        }
+
+        // Move the ship vertically
+        y -= mSpeed + GRAVITY;
+
+        // But don't let ship stray off screen
+        if (y < minY) {
+            y = minY;
+        }
+
+        if (y > maxY) {
+            y = maxY;
+        }
     }
 
     // Getters
@@ -29,7 +68,7 @@ public class PlayerShip {
     }
 
     public int getSpeed() {
-        return speed;
+        return mSpeed;
     }
 
     public int getX() {
