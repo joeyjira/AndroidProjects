a4254dbed974a40eaf390ca72d536f4798bdb016
Joey Jirasevijinda <joey.jira@gmail.com>
2018-06-22 17:14:09 -0700

    Added instance fields for enemy ships

---
+++ TappyDefender/app/src/main/java/com/septem/tappydefender/EnemyShip.java
@@ -0,0 +1,17 @@
+package com.septem.tappydefender;
+
+import android.graphics.Bitmap;
+
+public class EnemyShip {
+    private Bitmap bitmap;
+    private int x, y;
+    private int speed = 1;
+
+    // Detect enemies leaving the screen
+    private int maxX;
+    private int minX;
+
+    // Spawn enemies within screen bounds
+    private int maxY;
+    private int minY;
+}
