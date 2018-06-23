33536d082057c4b62c336d5919d86b9e62cf31d3
Joey Jirasevijinda <joey.jira@gmail.com>
2018-06-22 17:26:54 -0700

    added enemy ship to bitmap and set position

--- TappyDefender/app/src/main/java/com/septem/tappydefender/EnemyShip.java
+++ TappyDefender/app/src/main/java/com/septem/tappydefender/EnemyShip.java
@@ -1,6 +1,10 @@
 package com.septem.tappydefender;
 
+import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import java.util.Random;
 
 public class EnemyShip {
     private Bitmap bitmap;
@@ -14,4 +18,30 @@ public class EnemyShip {
     // Spawn enemies within screen bounds
     private int maxY;
     private int minY;
+
+    public EnemyShip(Context context, int screenX, int screenY) {
+        bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.enemy);
+        maxX= screenX;
+        maxY = screenY;
+        minX = 0;
+        minY = 0;
+
+        Random generator = new Random();
+        speed = generator.nextInt(6) + 10;
+
+        x = screenX;
+        y = generator.nextInt(maxY) - bitmap.getHeight();
+    }
+
+    public Bitmap getBitmap() {
+        return bitmap;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
 }
