bf9b3b8484150ff19eff339aa512b55d74401393
Joey Jirasevijinda <joey.jira@gmail.com>
2018-07-12 14:59:26 -0700

    search for specific crime with uuid

--- CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeLab.java
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeLab.java
@@ -52,8 +52,21 @@ public class CrimeLab {
     }
 
     public Crime getCrime(UUID id) {
+        CrimeCursorWrapper cursor = queryCrimes(
+                CrimeTable.Cols.UUID + " = ?",
+                new String[] { id.toString() }
+        );
 
-        return null;
+        try {
+            if (cursor.getCount() == 0) {
+                return null;
+            }
+
+            cursor.moveToFirst();
+            return cursor.getCrime();
+        } finally {
+            cursor.close();
+        }
     }
 
     public void addCrime(Crime c) {
